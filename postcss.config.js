68ebef0882589cc1ed3662ad4bcafddc61a03bc0
shivasen <shivasen22@gmail.com>
2025-11-20 16:17:05 +0530

    Fix merge conflicts in _redirects and postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 export default {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-=======
-export default {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
->>>>>>> 32dc51900d333c5af3e6ada76010dde71827b0fa
-  };
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
